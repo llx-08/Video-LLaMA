ce5eb23f14064f0a77a4908ec3e552c32537f712
llx-08 <2596671364@qq.com>
2024-03-21 22:26:40 +0800

    .

--- video_llama/datasets/datasets/video_instruct_dataset.py
+++ video_llama/datasets/datasets/video_instruct_dataset.py
@@ -323,7 +323,7 @@ def preprocess_multimodal(
     image_token_len = cur_token_len
     # conversation_list[0]["q"] = "<Video>"+DEFAULT_AUDIO_PATCH_TOKEN * image_token_len +"</Video> " + msg + conversation_list[0]["q"]
     conversation_list[0]["q"] = "<Video>"+DEFAULT_IMAGE_PATCH_TOKEN * image_token_len +"</Video> " + msg + \
-                                "Tell me what this audio is saying, give me the transcription of this video."
+                                "Transcript the audio."
     return [conversation_list]
 
 def _add_speaker_and_signal(header, source, get_conversation=True):
