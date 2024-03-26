06af32cf2f8cc79d862b310bdd48985c841c2c83
llx-08 <2596671364@qq.com>
2024-03-26 16:08:32 +0800

    .

--- train_script.sh
+++ train_script.sh
@@ -1,3 +1,3 @@
-nohup python train.py --cfg-path  ./train_configs/audiobranch_stage2_finetune.yaml
+nohup python train.py --cfg-path  ./train_configs/audiobranch_stage2_finetune.yaml > debug.log 2>&1 &
 
 #nohup torchrun --nproc_per_node=2 train.py --cfg-path  ./train_configs/audiobranch_stage2_finetune.yaml
